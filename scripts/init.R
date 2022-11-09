1a1573e59bdcdcd195d4090f4a8fc3adc99f6e8c
Lars Qviller <larsqviller@yahoo.com>
2022-11-09 13:05:01 +0100

    Update treatment choices
    Treatments are updated as follows:
    Choices with "behandle" "merdvis" etc are deleted. We choose instead from the type of treatment (non-medicinal etc), and all cages are selected as default.
    Non-medicinal treatment is deactivated before the salmon is above 1 kg.
    Feed treatment is only alowed once per cage/generation

--- scripts/init.R
+++ scripts/init.R
@@ -10,7 +10,7 @@ library(shinyjs)
 library(shinyBS)
 library(shinycssloaders)
 library(DT)
-
+Sys.setlocale(locale='no_NB.utf8')
 ## Load data
 Coef <- readRDS("Coef20211209.Rds")
 load("EnvListShort.Rdata")
